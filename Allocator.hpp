6261eed2783ec2ae5689779428293af3903bb8ee
nemuel (Game Two) <gametwoofficiel@gmail.com>
2021-01-04 10:56:31 +0100

    debut

---
+++ Allocator.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Allocator.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nepage-l <nepage-l@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/04 10:40:24 by nepage-l          #+#    #+#             */
+/*   Updated: 2021/01/04 10:43:58 by nepage-l         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+template<typename T>
+class Allocator {
+public : 
+    //    typedefs
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+
+public : 
+    //    convert an allocator<T> to allocator<U>
+    template<typename U>
+    struct rebind {
+        typedef Allocator<U> other;
+    };
+
+public : 
+    inline explicit Allocator() {}
+    inline ~Allocator() {}
+    inline explicit Allocator(Allocator const&) {}
+    template<typename U>
+    inline explicit Allocator(Allocator<U> const&) {}
+
+    //    address
+    inline pointer address(reference r) { return &r; }
+    inline const_pointer address(const_reference r) { return &r; }
+
+    //    memory allocation
+    inline pointer allocate(size_type cnt, 
+       typename std::allocator<void>::const_pointer = 0) { 
+      return reinterpret_cast<pointer>(::operator new(cnt * sizeof (T))); 
+    }
+    inline void deallocate(pointer p, size_type) { 
+        ::operator delete(p); 
+    }
+
+    //    size
+    inline size_type max_size() const { 
+        return std::numeric_limits<size_type>::max() / sizeof(T);
+ }
+
+    //    construction/destruction
+    inline void construct(pointer p, const T& t) { new(p) T(t); }
+    inline void destroy(pointer p) { p->~T(); }
+
+    inline bool operator==(Allocator const&) { return true; }
+    inline bool operator!=(Allocator const& a) { return !operator==(a); }
+};    //    end of class Allocator
